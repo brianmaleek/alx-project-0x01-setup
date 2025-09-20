18f7f1c72432c7bc697f30601d488a89b4b1b230
Brian Maleek <maleekmalik@gmail.com>
2025-09-21 00:38:58 +0300

    Add Users page: implement layout to display user details using UserCard component

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,30 @@
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+    return (
+        <div className="p-4">
+            <h1 className=" text-2xl font-semibold mb-4">
+                Our Users
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                {posts.map((user) => (
+                    <UserCard key={user.id} {...user} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const posts = await response.json()
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
+export default Users;
